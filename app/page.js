08fbd4f1d64a0b7e2e9702b842eda165f97473b5
Aayushi Sharma <aayushisharma20@navgurukul.org>
2023-11-01 12:46:37 +0530

    folder

--- app/page.js
+++ app/page.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import Input from './input'
-export default function Home() {
-  return (
-    
-      
-        <Input/>
-      
-  )
-}
-=======
-<<<<<<< HEAD
 import Input_box from './input_box.js'
 const Page=function(){
     return(
@@ -20,19 +8,4 @@ const Page=function(){
         </>
     );
 };
-=======
-"use client";
-import React, { useState } from "react";
-import Input_button from "./input_button.js";
-import Button from "./add_button.js";
-import Delete from "./delete.js";
-const Page = () => {
-    return (
-        <>
-            <Input_button />
-        </>
-    );
-};    
->>>>>>> f5e4646993dbcf3afce4a7992eff8566f3b8a84e
 export default Page;
->>>>>>> b302dde5f7a546852436c6f706fc8fc050013385
