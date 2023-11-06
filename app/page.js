5584d384d1071300c3692a900b34e55fe100c343
Archana Singh <archana21@navgurukul.org>
2023-11-06 18:09:35 +0530

    pull some file

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Input_box from './input_box.js'
 const Page=function(){
     return(
@@ -9,18 +8,4 @@ const Page=function(){
         </>
     );
 };
-=======
-"use client";
-import React, { useState } from "react";
-import Input_button from "./input_button.js";
-import Button from "./add_button.js";
-import Delete from "./delete.js";
-const Page = () => {
-    return (
-        <>
-            <Input_button />
-        </>
-    );
-};    
->>>>>>> f5e4646993dbcf3afce4a7992eff8566f3b8a84e
 export default Page;
