dc932c83abcb1880231611e6b5ef568976e3eb88
Deepanjali <deepanjali20@navgurukul.org>
2023-11-10 11:02:32 +0530

    no change

--- app/input_box.js
+++ app/input_box.js
@@ -5,6 +5,7 @@ import Priority from './priority.js'
 
 const Input_box=function(){
      const options = [
+   { label: '0', value: '0' }, 
    { label: '1', value: '1' },
    { label: '2', value: '2' },
    { label: '3', value: '3' },
@@ -57,6 +58,8 @@ const Input_box=function(){
                   <th>Work</th>
                   <th>Time</th>
                   <th>Priority</th>
+                  <th>Status</th>
+                  <th>Delete</th>
                 </tr>
               </thead>
               <tbody>
